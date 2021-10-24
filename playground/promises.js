664a2d8580033614d9b2e99a4ccdaf1440e1f976
OpenTech-Consult <opentech.consult@gmail.com>
2021-10-24 21:40:36 +0000

    return to the state of the last commit. I forget to create a branch

--- playground/promises.js
+++
@@ -1,7 +0,0 @@
-const myPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('IO operation running');
-    }, 2000)
-});
-
-myPromise.then(result => console.log(result))

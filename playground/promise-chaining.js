5ae4cde062f4e13cad4fedb1b915d28b61eda1ff
OpenTech-Consult <opentech.consult@gmail.com>
2021-10-27 14:58:03 +0000

    Async-await
    Explore how to use async/await syntax to easily deal with  asynchronous function that returns promises.

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -3,11 +3,23 @@ const User = require('../src/model/user')
 
 // 6171ed1c09e97976ecd2e4ad
 
-User.findByIdAndUpdate('6171ed1c09e97976ecd2e4ad', { age: 39 }).then((user) => {
-    console.log(user)
-    return User.countDocuments({ age: 39 })
-}).then((result) => {
-    console.log(result)
+// User.findByIdAndUpdate('6171ed1c09e97976ecd2e4ad', { age: 39 }).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({ age: 39 })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age })
+    const count = await User.countDocuments({ age })
+    return count
+}
+
+updateAgeAndCount('6171ed1c09e97976ecd2e4ad', 42).then((count) => {
+    console.log(count)
 }).catch((err) => {
-    console.log(err)
+    console.log(err);
 })

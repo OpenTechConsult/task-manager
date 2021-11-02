2dcacf33e1d3f04424ae759f96e34f37b5dc1f41
OpenTech-Consult <opentech.consult@gmail.com>
2021-11-02 13:22:01 +0000

    Logging in users using bcrypt and jwt
    installed jsonwebtoken module
    created a static method findByCredential that allows to fetch a user by its email and compare its hashed password by the one provided when login in using POST /users/login
    created an instance method generateAuthToken that allows us to generate a token and save that token to the user document
    created a field in the userSchema which is an array of tokens
    created the route handler for  POST  /users/login in the src/routers/user.js. The route handle takes in body of email and password  and use the method findByCredential created in src/model/user.js to find a user. It also generate the jwt token and send the user and the token generated
    explore how to create a middleware

--- src/model/user.js
+++ src/model/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const { Schema, model } = mongoose
 
@@ -24,6 +25,7 @@ const userSchema = new Schema({
     },
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase: true,
@@ -41,11 +43,37 @@ const userSchema = new Schema({
                 throw new Error('Age must be a positive number')
             }
         }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true,
+        }
+    }]
 
-    }
 })
 
-// create the pre save middleware
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+    return token
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+    if (!user){
+        throw new Error('Unable to login')
+    }
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) {
+        throw new Error('Unable to login')
+    }
+    return user
+}
+
+// hash the plain text password before saving.
 userSchema.pre('save', async function(next) {
     const user = this
     if (user.isModified('password')) {

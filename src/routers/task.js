915200e47ec5f91551900061517ecaa9daf91e22
OpenTech-Consult <opentech.consult@gmail.com>
2021-11-15 09:10:15 +0000

    Sorting data
    Enable sorting data option

--- src/routers/task.js
+++ src/routers/task.js
@@ -48,19 +48,26 @@ router.patch('/tasks/:id', auth, async (req, res) => {
 // fetch all tasks by owner
 // GET /tasks?completed =true 
 router.get('/tasks', auth, async (req , res) => {
+
     const match = {}
+    const sort = {}
+
     if (req.query.completed) {
         match.completed = req.query.completed === 'true'
     }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
     try {
-        // const tasks = await Task.find({ owner: req.user._id })
-        // console.log(tasks)
         const user = await req.user.populate({
             path: 'tasks',
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         })
         console.log(user.tasks)
@@ -81,7 +88,6 @@ router.get('/tasks/:id', auth, async (req, res) => {
     }
 
     try {
-        //const task = await Task.findById(_id)
         const task = await Task.findOne({ _id, owner: req.user._id})
         if (!task) {
             return res.status(404).send()

daf26dda44843038ce85874853ef2889b86fd7e0
OpenTech-Consult <opentech.consult@gmail.com>
2021-11-17 08:37:55 +0000

    Add feature to add image to the user profile Add a feature to delete a profile image if the user wants to
    closes #86

--- src/routers/user.js
+++ src/routers/user.js
@@ -91,7 +91,6 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 const upload = multer({
-    dest: 'avatars',
     limits: {
         fileSize: 1000000,
     }, 
@@ -103,10 +102,27 @@ const upload = multer({
     }
 })
 
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+router.post('/users/me/avatar',auth, upload.single('avatar'), async (req, res) => {
+    req.user.avatar = req.file.buffer
+    await req.user.save()
     res.send();
 }, (error, req, res, next) => {
     res.status(400).send({error: error.message})
 })
+
+// TODO: Setup route to delete avatar
+// 1. Setup DELETE /users/me/avatar
+// 2. Add authentication
+// 3. Set the field to undefined ans save the user and sending back a 200
+// 4. Test the work by creating a new request for task app in postman
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    try {
+        req.user.avatar = undefined
+        await req.user.save()
+        res.send()
+    } catch (err) {
+        res.status(500).send({error: err.message})
+    }
+});
 
 module.exports = router

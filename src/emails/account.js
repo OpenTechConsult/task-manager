65ce238c7343ff1f04c0a25ff3fa0e581a4fd405
OpenTech-Consult <opentech.consult@gmail.com>
2021-11-18 23:40:35 +0000

    Sending welcome and cancelation email
    created a function called sendWelcomeEmail. the function sends an email to the user once he create an account
    created a function called sendGoodbyeEmail to allow the system to send an email when the user unregister in the app
    
    Closes #95

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,17 +1,38 @@
 const sgMail = require('@sendgrid/mail')
 
-const sendgridAPIKEY = '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'
+const sendGridAPIKEY = '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'
+sgMail.setApiKey(sendGridAPIKEY)
 
-sgMail.setApiKey(sendgridAPIKEY)
-
-const msg = {
-    to: 'eagboka@otr.tg', // Change to your recipient
-    from: 'opentech.consult@gmail.com', // Change to your verified sender
-    subject: 'Sending with SendGrid is Fun',
-    text: 'Hi its me sandro ',
-    html: '<strong>Just use sendgrid to send you a message using node.js</strong>',
+const sendWelcomeEmail = (email, name) => {
+    const msg = {
+        to: email, // Change to your recipient
+        from: 'opentech.consult@gmail.com', // Change to your verified sender
+        subject: 'Sending with SendGrid is Fun',
+        text: `Welcome to the app, ${name}, let us know how you get along with the user app`
+    }
+    
+    sgMail.send(msg)
+        .then(() => { console.log('Email sent')})
+        .catch((err) => { console.error(err)})
 }
 
-sgMail.send(msg)
-    .then(() => { console.log('Email sent')})
-    .catch((err) => { console.error(err)})
+const sendGoodbyeEmail = (email, name) => {
+    const msg = {
+        to: email, // Change to your recipient
+        from: 'opentech.consult@gmail.com', // Change to your verified sender
+        subject: 'Sending with SendGrid is Fun',
+        text: `Goodbye, ${name}, is there anything that we can do to keep you onboard?`
+    }
+    sgMail.send(msg)
+        .then(() => { console.log('Goodbye Email sent')})
+        .catch((err) => { console.error(err)})
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendGoodbyeEmail
+}
+
+
+
+

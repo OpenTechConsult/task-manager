daa336a7ce17b32efa42ac79978f376c33763acc
OpenTech-Consult <opentech.consult@gmail.com>
2021-11-28 17:20:17 +0000

    Testing with Task Data
    Bring in Task model inside 'fixture/db.js'
    Also add a second user to the mix in db.js
    Add also three tasks
    
    Test that we should get all tasks for userOne
    Test also that a given user should not delete another user task

--- tests/task.test.js
+++ tests/task.test.js
@@ -1,6 +1,15 @@
 const request = require('supertest');
 const app = require('../src/app')
-const { userOneId, userOne, setupDatabase } = require('./fixtures/db')
+const { 
+    userOneId,
+    userOne, 
+    userTwoId,
+    userTwo,
+    taskOne,
+    taskTwo,
+    taskThree,
+    setupDatabase 
+} = require('./fixtures/db')
 
 // Load the task model
 const Task = require('../src/model/task')
@@ -19,3 +28,29 @@ test('Should create task for user', async () => {
     expect(task).not.toBeNull()
     expect(task.completed).toEqual(false)
 })
+
+
+test('Should get all tasks for userOne', async () => {
+    const response = await request(app)
+            .get('/tasks')
+            .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+            .send()
+            .expect(200)
+    expect(response.body).not.toBeNull()
+    expect(response.body.length).toEqual(2)
+})
+
+
+test('Should not delete other users tasks', async () => {
+    const response = await request(app)
+            .delete(`/tasks/${taskOne._id}`)
+            .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+            .send()
+            .expect(404)
+
+    const task = await Task.findById(taskOne._id)
+    expect(task).not.toBeNull()
+})
+
+
+
